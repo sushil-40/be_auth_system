cd6e26197f5f0dfe99848df3b95639b5c235dd3f
Sushil <grayson.9999@gmail.com>
2025-07-04 09:19:49 +1000

    hashing password using bcrypt and storing data to database

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,10 +1,17 @@
-export const insertNewUser = (req, res, error) => {
+import { createNewUser } from "../models/user/UserModel.js";
+import { hashPassword } from "../utils/bcrypt.js";
+
+export const insertNewUser = async (req, res, error) => {
   try {
     // to do signup process
 
     //recieve the user data
     //encrpty the password
+    const { password } = req.body;
+    req.body.password = hashPassword(password);
+
     //insert into DB
+    const user = await createNewUser(req.body);
     // create an unique user activation link and send to their email
 
     res.json({
